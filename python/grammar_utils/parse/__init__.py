5b98cba8d0a0f58859dafa6a6f428a235e133152
Sebastian Walter <sebastian.walter98@gmail.com>
2024-11-15 11:27:36 +0100

    make __init__.py in parse executable to test parsers

--- python/grammar_utils/parse/__init__.py
+++ python/grammar_utils/parse/__init__.py
@@ -1,3 +1,6 @@
+import argparse
+from pprint import pprint
+
 from grammar_utils._internal import LR1Parser  # noqa
 from grammar_utils.grammars import load_grammar_and_lexer
 
@@ -12,3 +15,22 @@ def load_lr1_parser(name: str) -> tuple[str, str]:
 
     """
     return LR1Parser.from_grammar_and_lexer(*load_grammar_and_lexer(name))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("grammar", type=str, help="The grammar to load")
+    parser.add_argument("lexer", type=str, help="The lexer to load")
+    parser.add_argument("input", type=str, help="The input to parse")
+    parser.add_argument(
+        "-p", "--prefix", action="store_true", help="Use prefix parsing"
+    )
+    args = parser.parse_args()
+    parser = LR1Parser.from_files(args.grammar, args.lexer)
+    if args.prefix:
+        tree, rest = parser.prefix_parse(args.input.encode())
+        pprint(tree)
+        pprint(rest)
+    else:
+        tree = parser.parse(args.input)
+        pprint(tree)
